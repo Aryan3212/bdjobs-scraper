aa8d712a8a7f3d1fa6249b1ee890719ed3878903
Aryan Rahman <57558971+Aryan3212@users.noreply.github.com>
2023-12-01 12:24:25 +0600

    chore: setup testing module

---
+++ tests/test.py
@@ -0,0 +1,9 @@
+import unittest
+import scraper
+
+class TestBDJobsScraper(unittest.TestCase):
+    def test_scraper(self):
+       self.assertEqual(2, 2)
+
+if __name__ == '__main__':
+    unittest.main()
